3aff28cce0e7b5fc2a8f107f8bab47cc9642839e
Marija <marijav871@gmail.com>
2020-11-20 18:27:38 +0100

    Kostur MVC

---
+++ src/test/java/com/minterm/rentacar/rentacar/RentacarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minterm.rentacar.rentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
