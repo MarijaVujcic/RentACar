48ada534d49dc79247d87329eb6e4a9d2a25003d
zeljkotopic <zeljkot98@gmail.com>
2020-11-22 22:55:21 +0100

    Class Locations and Cars are done

---
+++ src/main/java/com/minterm/rentacar/Model/Locations.java
@@ -0,0 +1,37 @@
+public class Locations{
+    int Id;
+    String city;
+    String address;
+    String region;
+    String country;
+}
+public int getId(){
+    return Id;
+}
+public String getCity(){
+    return city;
+}
+public String getAddress(){
+    return address;
+}
+public String getRegion(){
+    return region;
+}
+public String getCountry(){
+    return country;
+}
+public void setId(int newId){
+    this.Id = newId;
+}
+public void setCity(String newCity){
+    this.city = newCity;
+}
+public void setAddress(String newAddress){
+    this.address = newAddress;
+}
+public void setRegion(String newRegion){
+    this.region = newRegion;
+}
+public void setCountry(String newCountry){
+    this.country = newCountry;
+}
