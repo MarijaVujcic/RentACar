8c139cc3b3e4ac9a75bc2fa33af86d855006120f
teacurkovic <72707605+teacurkovic@users.noreply.github.com>
2020-11-23 23:00:51 +0100

    Add files via upload

---
+++ ReservationStatus.java
@@ -0,0 +1,5 @@
+package com.minterm.rentacar.Model;
+
+public class ReservationStatus {
+    
+}
---
+++ User.java
@@ -0,0 +1,11 @@
+package com.minterm.rentacar.Model;
+
+public class User {
+    int Id;
+    String email;
+    String name;
+    String lastName;
+    String address;
+    String password;  
+    
+}
