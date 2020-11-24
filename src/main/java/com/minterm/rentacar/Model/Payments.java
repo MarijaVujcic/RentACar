a459751b16ca8ea0742debab988a99f0a586f1d5
Bozena Tesija <bozenatesija@gmail.com>
2020-11-24 23:00:29 +0100

    new

---
+++ src/main/java/com/minterm/rentacar/Model/Payments.java
@@ -0,0 +1,28 @@
+package com.minterm.rentacar.Model;
+
+public class Payments() {
+    int ID;
+    String TypesOfPayments;
+
+    
+    public int getID(){
+        return this.ID;
+    }
+
+    public Payments(int ID, String TypesOfPayments){
+        this.ID = ID;
+        this.TypesOfPayments = TypesOfPayments;
+    }
+
+    public void setID(int newID){
+        this.ID = newID;
+    }
+    
+    public String TypesOfPayments(){
+        return this.TypesOfPayments;
+    }
+
+    public void setTypesOfPayments(String newTypesOfPayments){
+        this.TypesOfPayments = newTypesOfPayments;
+    }
+}
---
+++ src/main/java/com/minterm/rentacar/Model/Reservations.java
@@ -0,0 +1,96 @@
+package com.minterm.rentacar.Model;
+import java.util.Date;
+
+public class Reservations(){
+    int CarID;
+    int UserID;
+    Date StartDateTime;
+    Date EndDateTime;
+    String PickUpLocation;
+    String LeavingLocation;
+    int TotalPrice;
+    String TypesOfPayments;
+    Date LastDayOfCancellation;
+    String ReservationChanges;
+    String Insurance;
+
+
+    public int getCarID(){
+        return this.CarID;
+    }
+
+    public int getUserID(){
+        return this.UserID;
+    }
+
+    public Reservations(int CarID, int UserID, Date StartDateTime, Date EndDateTime, String PickUpLocation, String LeavingLocation,
+    int TotalPrice, String TypesOfPayments, Date LastDayOfCancellation, String ReservationChanges, String Insurance){
+        this.CarID = CarID;
+        this.UserID = UserID;
+        this.StartDateTime = StartDateTime;
+        this.EndDateTime = EndDateTime;
+        this.PickUpLocation = PickUpLocation;
+        this.LeavingLocation = LeavingLocation;
+        this.TotalPrice =TotalPrice;
+        this.TypesOfPayments = TypesOfPayments;
+        this.LastDayOfCancellation = LastDayOfCancellation;
+        this.ReservationChanges = ReservationChanges;
+        this.Insurance = Insurance;
+    }
+
+    public void setCarID(int newCarID){
+        this.CarID = newCarID;
+    }
+
+    public void setUserID(int newUserID){
+        this.UserID = newUserID;
+    }
+
+    public String getPickUpLocation(){
+        return this.PickUpLocation;
+    }
+
+    public void setPickUpLocation(String newPickUpLocation){
+        this.PickUpLocation = newPickUpLocation;
+    }
+
+    public String getLeavingLocation(){
+        return this.LeavingLocation;
+    }
+
+    public void setLeavingLocation(String newLivingLocation){
+        this.LeavingLocation = newLivingLocation;
+    }
+
+    public int getTotalPrice(){
+        return this.TotalPrice;
+    }
+
+    public void setTotalPrice(int newTotalPrice){
+        this.TotalPrice = newTotalPrice;
+    }
+
+    public String getTypesOfPayments(){
+        return this.TypesOfPayments;
+    }
+
+    public void setTypesOfPayments(String newTypesOfPayments){
+        this.TypesOfPayments = newTypesOfPayments;
+    }
+
+    public String getReservationChanges(){
+        return this.ReservationChanges;
+    }
+
+    public void setReservationChanges(String newReservationChanges){
+        this.ReservationChanges = newReservationChanges;
+    }
+
+    public String getInsurance(){
+        return this.Insurance;
+    }
+
+    public void setInsurance(String newInsurance){
+        this.Insurance = newInsurance;
+    }
+}
