43864ee97a5cbd2d1d903d42ba9c7252228e8cdd
zeljkotopic <zeljkot98@gmail.com>
2020-11-24 18:39:20 +0100

    popravljeno

--- src/main/java/com/minterm/rentacar/Model/Cars.java
+++ src/main/java/com/minterm/rentacar/Model/Cars.java
@@ -10,58 +10,59 @@ public class Cars {
     Bool automatic;
     int num_of_doors;
     Bool reserved;
-}
-public int getId(){
-    return this.Id;
-}
-public String getBrand(){
-    return this.brand;
-}
-public String getModel(){
-    return this.model;
-}
-public int getNum_of_sitts(){
-    return this.num_of_sitts;
-}
-public float getPrice(){
-    return this.price;
-}
-public String getDescription(){
-    return this.description;
-}
-public Bool getAutomatic(){
-    return this.automatic;
-}
-public int getNum_of_doors(){
-    return this.num_of_doors;
-}
-public Bool getReserved(){
-    return this.reserved;
-}
-public void setId(int newId){((
-    this.Id = newId;
-}
-public void setBrand(String newBrand){
-    this.brand = newBrand;
-}
-public void setModel(String newModel){
-    this.model = newModel;
-}
-public void setNum_of_sitts(int newNum_of_sitts){
-    this.num_of_sitts = newNum_of_sitts;
-}
-public void setPrice(float newPrice){
-    this.price = newPrice;
-}
-public void setDescription(String newDescription){
-    this.description = newDescription;
-}
-public void setAutomatic(Bool newAutomatic){
-    this.model = newAutomatic;
-}
-public void setNum_of_doors(int newNum_of_doors){
-    this.num_of_doors = newNum_of_doors;
-}
-public void setReserved(Bool newReserved){
-    this.reserved = newReserved;
+
+    public int getId(){
+        return this.Id;
+    }
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public int getNum_of_sitts(){
+        return this.num_of_sitts;
+    }
+    public float getPrice(){
+        return this.price;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public Bool getAutomatic(){
+        return this.automatic;
+    }
+    public int getNum_of_doors(){
+        return this.num_of_doors;
+    }
+    public Bool getReserved(){
+        return this.reserved;
+    }
+    public void setId(int newId){((
+        this.Id = newId;
+    }
+    public void setBrand(String newBrand){
+        this.brand = newBrand;
+    }
+    public void setModel(String newModel){
+        this.model = newModel;
+    }
+    public void setNum_of_sitts(int newNum_of_sitts){
+        this.num_of_sitts = newNum_of_sitts;
+    }
+    public void setPrice(float newPrice){
+        this.price = newPrice;
+    }
+    public void setDescription(String newDescription){
+        this.description = newDescription;
+    }
+    public void setAutomatic(Bool newAutomatic){
+        this.model = newAutomatic;
+    }
+    public void setNum_of_doors(int newNum_of_doors){
+        this.num_of_doors = newNum_of_doors;
+    }
+    public void setReserved(Bool newReserved){
+        this.reserved = newReserved;
+    }
 }
--- src/main/java/com/minterm/rentacar/Model/Locations.java
+++ src/main/java/com/minterm/rentacar/Model/Locations.java
@@ -1,40 +1,41 @@
 package com.minterm.rentacar.Model;
 
 @Entity
-public class Locations{
+public class Locations {
     int Id;
     String city;
     String address;
     String region;
     String country;
-}
-public int getId(){
-    return this.Id;
-}
-public String getCity(){
-    return this.city;
-}
-public String getAddress(){
-    return this.address;
-}
-public String getRegion(){
-    return this.region;
-}
-public String getCountry(){
-    return this.country;
-}
-public void setId(int newId){
-    this.Id = newId;
-}
-public void setCity(String newCity){
-    this.city = newCity;
-}
-public void setAddress(String newAddress){
-    this.address = newAddress;
-}
-public void setRegion(String newRegion){
-    this.region = newRegion;
-}
-public void setCountry(String newCountry){
-    this.country = newCountry;
+
+    public int getId(){
+        return this.Id;
+    }
+    public String getCity(){
+        return this.city;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public String getRegion(){
+        return this.region;
+    }
+    public String getCountry(){
+        return this.country;
+    }
+    public void setId(int newId){
+        this.Id = newId;
+    }
+    public void setCity(String newCity){
+        this.city = newCity;
+    }
+    public void setAddress(String newAddress){
+        this.address = newAddress;
+    }
+    public void setRegion(String newRegion){
+        this.region = newRegion;
+    }
+    public void setCountry(String newCountry){
+        this.country = newCountry;
+    }
 }
